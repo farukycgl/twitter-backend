84e55f8e20a2353832dfeb5735b2d7451168e88d
farukycgl <faruk.kuy@gmail.com>
2025-05-11 01:23:06 +0300

    config package, SecurityConfig classes created

---
+++ twitter-backend/src/main/java/com/tryst/twitter_backend/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.tryst.twitter_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().permitAll();
+                })
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
+}
